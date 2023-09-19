484f25c618c7eaf13df78a75e40c16185a5a8405
bedirhandogan <bedirhandgn14@gmail.com>
2023-09-19 13:49:08 +0300

    refactor: some changes

--- parser.go
+++ parser.go
@@ -28,14 +28,16 @@ func ParseTime(duration string) TimePeriod {
 	for _, part := range parts {
 		keyValue := strings.Split(part, ":") // [d: 0] [h: 0] [m: 0] [s: 0]
 
-		if len(keyValue) == 2 {
-			key := keyValue[0]
-			value := keyValue[1]
+		if len(keyValue) != 2 {
+			continue
+		}
 
-			valInt, err := strconv.Atoi(value) // convert int
-			if err == nil {
-				tp[key] = valInt
-			}
+		key := keyValue[0]
+		value := keyValue[1]
+
+		valInt, err := strconv.Atoi(value) // convert int
+		if err == nil {
+			tp[key] = valInt
 		}
 	}
 
