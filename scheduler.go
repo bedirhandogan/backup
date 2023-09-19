b69c0c02d7821c80b2dd15819a9663fe886a183b
bedirhandogan <bedirhandgn14@gmail.com>
2023-09-19 11:22:37 +0300

    add: task scheduler function

---
+++ scheduler.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"time"
+)
+
+type Callback func() error
+
+func TaskScheduler(callback Callback, day, hour, min, sec int) {
+	interval := time.Duration(day*24*int(time.Hour) + hour*int(time.Hour) + min*int(time.Minute) + sec*int(time.Second))
+	ticker := time.NewTicker(interval)
+
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			err := callback()
+
+			if err != nil {
+				return
+			}
+		}
+	}
+}
